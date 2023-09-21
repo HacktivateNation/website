254b14c9f14e949831580233c28ab424b3835859
MTanner <tannerwebdevelop@gmail.com>
2023-09-21 00:09:23 -0400

    Merging in Guru's work

--- eleventy.js
+++ eleventy.js
@@ -12,13 +12,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   // eleventyConfig.addPassthroughCopy({"./node_modules/material-icons/css/material-icons.min.css": "./css/lib/material-icons.min.css"});
 
-  // Use Nunjucks for all template files
-  // eleventyConfig.setTemplateFormats("html,njk");
-  // eleventyConfig.setLibrary("njk", require("nunjucks"));
-  // eleventyConfig.markdownTemplateEngine = "njk";
-  // eleventyConfig.dataTemplateEngine = "njk";
-  // eleventyConfig.htmlTemplateEngine = "njk";
-
   return {
     // Set the directories for include and layout files
     dir: {
